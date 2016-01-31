f633af062cf0c3de4532b7ce8756677d0f938a25
NicholasPiano <nicholas.d.piano@gmail.com>
2016-01-31 17:34:04 +0000

    test

--- woot/apps/expt/management/commands/test.py
+++ woot/apps/expt/management/commands/test.py
@@ -1,13 +1,16 @@
-# expt.command:
+# expt.command: data
 
 # django
 from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
 
 # local
-
+from apps.img.models import Composite
 
 # util
-
+import numpy as np
+from skimage import exposure
+import matplotlib.pyplot as plt
 
 spacer = ' ' *	20
 
@@ -21,4 +24,18 @@ class Command(BaseCommand):
 	help = ''
 
 	def handle(self, *args, **options):
-		pass
+		composite = Composite.objects.get()
+
+		zmean_gon = composite.gons.get(channel__name='-zmean', t=0)
+		zmod_gon = composite.gons.get(channel__name='-zmod', t=0)
+
+		zmean = zmean_gon.load()
+		zmod = zmod_gon.load()
+
+		zunique = np.zeros(zmean.shape)
+		for unique in np.unique(zmod):
+			zunique[zmod==unique] = np.max(zmean[zmod==unique]) / np.sum(zmean)
+
+		# zunique = gf(zunique, sigma=3)
+		plt.imshow(zunique, cmap='Greys_r')
+		plt.show()
