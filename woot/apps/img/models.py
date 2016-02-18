e644ac8a454c9370e61b561eb2e765baf9de8fec
NicholasPiano <nicholas.d.piano@gmail.com>
2016-02-18 13:49:58 +0000

    make unique key non-unique and non-random

--- woot/apps/img/models.py
+++ woot/apps/img/models.py
@@ -478,8 +478,8 @@ class Channel(models.Model):
 		return '{} > {}'.format(self.composite.id_token, self.name)
 
 	def segment_setup(self, marker_channel_name='-zunique'):
-		unique = random_string() # defines a single identifier for this run
-		unique_key = '{}{}-{}'.format(marker_channel_name, self.name, unique)
+		unique = 'UNIQUE' # defines a single identifier for this run
+		unique_key = '{}{}-{}UK'.format(marker_channel_name, self.name, unique)
 
 		# setup
 		print('getting marker channel')
