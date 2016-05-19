b0c7c70c62d5876e1ae4298f42512373662859a2
NicholasPiano <nicholas.d.piano@gmail.com>
2016-05-19 16:55:50 +0100

    try one series

--- correct.sh
+++ correct.sh
@@ -1,66 +1,28 @@
-# python manage.py new_base_path --expt=20_11_15;
-# python manage.py apply_corrections --expt=20_11_15 --series=7;
-# python manage.py export --expt=20_11_15 --series=7;
-# python manage.py apply_corrections --expt=20_11_15 --series=8;
-# python manage.py export --expt=20_11_15 --series=8;
-
-python manage.py apply_corrections --expt=03_03_16_MB4175 --series=8;
-python manage.py export --expt=03_03_16_MB4175 --series=8;
-python manage.py apply_corrections --expt=03_03_16_MB4175 --series=9;
-python manage.py export --expt=03_03_16_MB4175 --series=9;
+# python manage.py apply_corrections --expt=03_03_16_MB4175 --series=8;
+# python manage.py export --expt=03_03_16_MB4175 --series=8;
+# python manage.py apply_corrections --expt=03_03_16_MB4175 --series=9;
+# python manage.py export --expt=03_03_16_MB4175 --series=9;
 #
 python manage.py apply_corrections --expt=11_03_16_MB4175 --series=5;
 python manage.py export --expt=11_03_16_MB4175 --series=5;
 #
-python manage.py apply_corrections --expt=120216HUVEC --series=6;
-python manage.py export --expt=120216HUVEC --series=6;
-
-python manage.py apply_corrections --expt=18_03_16_MB4175 --series=6;
-python manage.py export --expt=18_03_16_MB4175 --series=6 --flip_top;
-python manage.py apply_corrections --expt=18_03_16_MB4175 --series=7;
-python manage.py export --expt=18_03_16_MB4175 --series=7 --flip_top;
-
-# python manage.py apply_corrections --expt=20_11_15 --series=10;
-# python manage.py export --expt=20_11_15 --series=10;
+# python manage.py apply_corrections --expt=120216HUVEC --series=6;
+# python manage.py export --expt=120216HUVEC --series=6;
 #
-# python manage.py new_base_path --expt=16_12_15;
-# python manage.py apply_corrections --expt=16_12_15 --series=5;
-# python manage.py export --expt=16_12_15 --series=5 --flip_top;
-# python manage.py apply_corrections --expt=16_12_15 --series=6;
-# python manage.py export --expt=16_12_15 --series=6 --flip_top;
-# python manage.py apply_corrections --expt=16_12_15 --series=7;
-# python manage.py export --expt=16_12_15 --series=7 --flip_top;
+# python manage.py apply_corrections --expt=18_03_16_MB4175 --series=6;
+# python manage.py export --expt=18_03_16_MB4175 --series=6 --flip_top;
+# python manage.py apply_corrections --expt=18_03_16_MB4175 --series=7;
+# python manage.py export --expt=18_03_16_MB4175 --series=7 --flip_top;
 #
-# python manage.py new_base_path --expt=13_11_15;
-# python manage.py apply_corrections --expt=13_11_15 --series=13;
-# python manage.py export --expt=13_11_15 --series=13;
-# python manage.py apply_corrections --expt=13_11_15 --series=14;
-# python manage.py export --expt=13_11_15 --series=14;
-# python manage.py apply_corrections --expt=13_11_15 --series=15;
-# python manage.py export --expt=13_11_15 --series=15;
-# python manage.py apply_corrections --expt=13_11_15 --series=16;
-# python manage.py export --expt=13_11_15 --series=16;
+# python manage.py apply_corrections --expt=08_04_16_HUVEC_MB4175 --series=12;
+# python manage.py export --expt=08_04_16_HUVEC_MB4175 --series=12 --flip_top;
+# python manage.py apply_corrections --expt=08_04_16_HUVEC_MB4175 --series=13;
+# python manage.py export --expt=08_04_16_HUVEC_MB4175 --series=13 --flip_top;
 #
-# python manage.py new_base_path --expt=27_11_15;
-# python manage.py apply_corrections --expt=27_11_15 --series=7;
-# python manage.py export --expt=27_11_15 --series=7;
-# python manage.py apply_corrections --expt=27_11_15 --series=8;
-# python manage.py export --expt=27_11_15 --series=8;
+# python manage.py apply_corrections --expt=12_02_16_HUVEC_MB4175 --series=7;
+# python manage.py export --expt=12_02_16_HUVEC_MB4175 --series=7;
 #
-# python manage.py new_base_path --expt=11_12_15;
-# python manage.py apply_corrections --expt=11_12_15 --series=6;
-# python manage.py export --expt=11_12_15 --series=6 --flip_top;
-# python manage.py apply_corrections --expt=11_12_15 --series=7;
-# python manage.py export --expt=11_12_15 --series=7 --flip_top;
-# python manage.py apply_corrections --expt=11_12_15 --series=8;
-# python manage.py export --expt=11_12_15 --series=8 --flip_top;
-# python manage.py apply_corrections --expt=11_12_15 --series=9;
-# python manage.py export --expt=11_12_15 --series=9 --flip_top;
-#
-# python manage.py new_base_path --expt=04_12_15;
-# python manage.py apply_corrections --expt=04_12_15 --series=7;
-# python manage.py export --expt=04_12_15 --series=7 --flip_top;
-# python manage.py apply_corrections --expt=04_12_15 --series=8;
-# python manage.py export --expt=04_12_15 --series=8 --flip_top;
-# python manage.py apply_corrections --expt=04_12_15 --series=9;
-# python manage.py export --expt=04_12_15 --series=9 --flip_top;
+# python manage.py apply_corrections --expt=21_02_16_HUVEC_MB4175 --series=16;
+# python manage.py export --expt=21_02_16_HUVEC_MB4175 --series=16 --flip_top;
+# python manage.py apply_corrections --expt=21_02_16_HUVEC_MB4175 --series=17;
+# python manage.py export --expt=21_02_16_HUVEC_MB4175 --series=17 --flip_top;
